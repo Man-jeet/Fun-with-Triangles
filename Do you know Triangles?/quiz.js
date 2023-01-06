51b30467faf561873f3a26007cad7e9aab63d9e6
Manjeet <104201388+Man-jeet@users.noreply.github.com>
2023-01-06 21:21:07 +0530

    Update quiz.js

--- Do you know Triangles?/quiz.js
+++ Do you know Triangles?/quiz.js
@@ -1,20 +1,27 @@
 const quizForm = document.querySelector("#quiz-form");
-const submitAnsBtn = document.querySelector("#submit-ans-btn")
+const submitAnsBtn = document.querySelector("#submit-ans-btn");
+const outputA1 = document.querySelector("#output");
 
-const outputA1=document.querySelector("#output");
-const correctAnswers=["90","right angled"];
+const correctAnswers = [
+  "90",
+  "right angled",
+  "180 degrees",
+  "equilateral",
+  "edges",
+];  
+submitAnsBtn.addEventListener("click", calculateScore);
 
-submitAnsBtn.addEventListener("click",calculateScore);
-
-function calculateScore(){
-    let score=0;
-    index=0;
-    const formResult=new FormData(quizForm);
-    for (let value of formResult.values()){
-        if(value===correctAnswers[index]){
-            score=score+1;
-        }
-        index=index+1;
+function calculateScore() {
+  let score = 0;
+  index = 0;
+  const formResult = new FormData(quizForm);
+  console.log(formResult);
+  for (let value of formResult.values()) {
+    if (value === correctAnswers[index]) {
+        console.log(value,correctAnswers[index])
+      score = score + 1;
     }
-    outputA1.innerText = "The score is " + score ;
+    index = index + 1;
+  }
+  outputA1.innerText = "The score is " + score;
 }
