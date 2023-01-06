69352f122989c2c0353cd07e23487fc60045d7ac
Manjeet <104201388+Man-jeet@users.noreply.github.com>
2023-01-06 21:16:57 +0530

    Update area.js

--- Do you know Triangles?/area.js
+++ Do you know Triangles?/area.js
@@ -1,4 +1,5 @@
 const angleSides = document.querySelectorAll("input");
+// console.log(angleSides)
 const calculate = document.querySelector("#calculate-btn");
 const output= document.querySelector("#output")
 
@@ -7,14 +8,15 @@ calculate.addEventListener("click",calculateArea);
 
 
 function calculateArea(){
-    const area = 
-    sumOfSides(Number(angleSides[0].value),
-    Number(angleSides[1].value));
-    var areaOfTriangle = ((1/2)*area)
+    let side1  = Number(angleSides[0].value);
+    let side2 = Number(angleSides[1].value);
+    // console.log(typeof side1)
+    if (side1 > 0 && side2 > 0){
+        let areaOfTriangle = ((1/2)*side1* side2)
+        output.innerText="The area of the triangle is "+ String(areaOfTriangle)+"cm²"
+    }
+    else{
+        output.innerText = "Enter the positive value "
+    }
     
-    output.innerText="The area of the triangle is "+ String(areaOfTriangle)+"cm²"
-}
-
-function sumOfSides(){
-    return Number(angleSides[0].value) * Number(angleSides[1].value)
 }
